40a848d11ea13fe6aabf9a5fd56008b32ae5ad9a
Sai Yashwanth <taddishetty34@gmail.com>
2024-01-17 00:13:57 +0530

    Add: structs halfway through

---
+++ go_with_tests/5structs/go.mod
@@ -0,0 +1,3 @@
+module ttd/structs
+
+go 1.20
---
+++ go_with_tests/5structs/structs_test.go
@@ -0,0 +1,40 @@
+package structs
+
+
+
+import (
+	"testing"
+)
+func TestPerimeter(t *testing.T){
+	rectangle := Rectangle{10.0, 10.0}
+
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want{
+		t.Errorf("got %.2f, want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T){
+	t.Run("Rectangles", func(t *testing.T){
+		rectangle := Rectangle{12.0, 6.0}
+		got := Area(rectangle)
+		want := 72.0
+
+		if got != want{
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+	})
+
+	t.Run("Circles", func(t *testing.T){
+		circle := Circle{10}
+		got := Area(circle)
+		want := 314.1592653589793
+
+		if got != want{
+			t.Errorf("got %g, want %g", got, want)
+		}
+	})
+	
+}
